72469b89e1dc03d366e776799967cf6169ae4fcf
Kristóf Poduszló <kripod@protonmail.com>
2017-11-29 01:13:11 +0100

    Add videos section with an example

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,34 +2,82 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Container from '../components/Container';
 
-const IndexPage = ({ data }) => (
-  <div
-    css={{
-      background: 'linear-gradient(227.5deg, #00e676, #009688)',
-      height: '100vh',
-      display: 'flex',
-      alignItems: 'center',
-    }}
-  >
-    <Container
+const VideosSection = () => (
+  <div id="videos">
+    <h1>Videók</h1>
+
+    <article
       css={{
-        color: 'white',
+        display: 'flex',
+        alignItems: 'center',
+        margin: '-2rem',
 
-        '& h1': { fontSize: '3rem' },
-        '& h2': { fontSize: '2rem' },
-        '& h3': { fontSize: '1rem' },
-
-        '@media (min-width: 576px)': {
-          '& h1': { fontSize: '10vmin' },
-          '& h2': { fontSize: '5vmin' },
-          '& h3': { fontSize: '3vmin' },
+        '& > *': {
+          margin: '2rem',
         },
       }}
     >
-      <h1 css={{ wordSpacing: '100vw' }}>{data.site.siteMetadata.title}</h1>
-      <h2>2018. április 18.</h2>
-      <h2>BME I épület</h2>
-      <h3>Budapest, Magyar tudósok körútja 2, 1117</h3>
+      <div>
+        <h2>Satellite navigation on new waves</h2>
+        <p>
+          The European satellite navigation infrastructures EGNOS and Galileo are up and running.
+          Satellite navigation will facilitate autonomous driving, support to the
+          Internet-of-Things, advanced logistics, asset management and traffic management in sectors
+          like railways and aviation.
+        </p>
+      </div>
+
+      <video
+        src="http://coding.sch.bme.hu:8080/bss_vagott_web_16a9_HD/high_quality/simonyikonf2017_IB028_blokk1_verhoef_hq_HD.mp4"
+        controls
+        css={{
+          flexBasis: '80%',
+        }}
+      />
+    </article>
+  </div>
+);
+
+const IndexPage = ({ data }) => (
+  <div>
+    <div
+      css={{
+        background: 'linear-gradient(227.5deg, #00e676, #009688)',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      <Container
+        css={{
+          color: 'white',
+
+          '& h1': { fontSize: '3rem' },
+          '& h2': { fontSize: '2rem' },
+          '& h3': { fontSize: '1rem' },
+
+          '@media (min-width: 576px)': {
+            '& h1': { fontSize: '10vmin' },
+            '& h2': { fontSize: '5vmin' },
+            '& h3': { fontSize: '3vmin' },
+          },
+        }}
+      >
+        <h1 css={{ wordSpacing: '100vw' }}>{data.site.siteMetadata.title}</h1>
+        <h2>2018. április 18.</h2>
+        <h2>BME I épület</h2>
+        <h3>Budapest, Magyar tudósok körútja 2, 1117</h3>
+      </Container>
+    </div>
+
+    <Container
+      css={{
+        '& h1': {
+          textAlign: 'center',
+        },
+      }}
+    >
+      <VideosSection />
     </Container>
   </div>
 );
