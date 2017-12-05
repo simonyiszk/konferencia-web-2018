ec091d89e274e5960afb11f7aa36f73100410b13
Kristóf Poduszló <kripod@protonmail.com>
2017-12-05 11:58:05 +0100

    Use react-headroom for the header

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -2,6 +2,7 @@ import Link from 'gatsby-link';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { css, injectGlobal } from 'react-emotion';
+import Headroom from 'react-headroom';
 import Helmet from 'react-helmet';
 import FaChevronDown from 'react-icons/lib/fa/chevron-down';
 import FaChevronUp from 'react-icons/lib/fa/chevron-up';
@@ -67,121 +68,138 @@ class Header extends React.Component {
 
   render() {
     return (
-      <header
-        className={css`
-          position: fixed;
-          width: 100%;
-          color: white;
-          background: #009688;
-          z-index: 9;
-        `}
-      >
-        <Container
+      <header>
+        <Headroom
           className={css`
-            display: flex;
-            flex-wrap: wrap;
-            align-items: center;
+            position: absolute;
+            width: 100%;
+            color: white;
 
-            & a {
-              display: inline-block;
-              padding: 0.5rem;
-              margin: 1rem -0.5rem;
+            & .headroom--unfixed {
+              & > div {
+                background: transparent;
+              }
             }
           `}
         >
           <div
             className={css`
-              flex: 1;
+              background: #009688;
+              transition: background 0.5s;
             `}
           >
-            <Link to="/#home" onClick={() => this.setState({ isMenuOpen: false })}>
-              Főoldal
-            </Link>
-          </div>
-
-          <input
-            id="menu-checkbox"
-            name="isMenuOpen"
-            type="checkbox"
-            checked={this.state.isMenuOpen}
-            onChange={this.handleInputChange}
-            className={css`
-              display: none;
-
-              @media (max-width: 768px) {
-                &:not(:checked) {
-                  & + label > svg:nth-child(1) {
-                    display: none;
-                  }
-
-                  & ~ nav > ul {
-                    display: none;
-                  }
-                }
-
-                &:checked {
-                  & + label > svg:nth-child(2) {
-                    display: none;
-                  }
-
-                  & ~ nav > ul {
-                    display: block;
-                  }
-                }
-              }
-            `}
-          />
-
-          {/* eslint-disable jsx-a11y/label-has-for */}
-          <label
-            htmlFor="menu-checkbox"
-            className={css`
-              cursor: pointer;
-              user-select: none;
-              padding: 0.5rem;
-              margin: -0.5rem;
-
-              ${mediaQueries.medium`
-                display: none;
-              `};
-            `}
-          >
-            {/* eslint-enable jsx-a11y/label-has-for */}
-            <FaChevronUp />
-            <FaChevronDown />
-          </label>
-
-          <nav
-            className={css`
-              @media (max-width: 768px) {
-                width: 100%; /* Wrap component onto a new line */
-              }
-            `}
-          >
-            <ul
+            <Container
               className={css`
                 display: flex;
-                list-style-type: none;
-                padding: 0;
-                margin: 0;
+                flex-wrap: wrap;
+                align-items: center;
 
-                ${mediaQueries.medium`
+                & a {
+                  display: inline-block;
+                  padding: 0.5rem;
+                  margin: 1rem -0.5rem;
+                }
+              `}
+            >
+              <div
+                className={css`
+                  flex: 1;
+                `}
+              >
+                <Link to="/#home" onClick={() => this.setState({ isMenuOpen: false })}>
+                  Főoldal
+                </Link>
+              </div>
+
+              <input
+                id="menu-checkbox"
+                name="isMenuOpen"
+                type="checkbox"
+                checked={this.state.isMenuOpen}
+                onChange={this.handleInputChange}
+                className={css`
+                  display: none;
+
+                  @media (max-width: 768px) {
+                    &:not(:checked) {
+                      & + label > svg:nth-child(1) {
+                        display: none;
+                      }
+
+                      & ~ nav > ul {
+                        display: none;
+                      }
+                    }
+
+                    &:checked {
+                      & + label > svg:nth-child(2) {
+                        display: none;
+                      }
+
+                      & ~ nav > ul {
+                        display: block;
+                      }
+                    }
+                  }
+                `}
+              />
+
+              {/* eslint-disable jsx-a11y/label-has-for */}
+              <label
+                htmlFor="menu-checkbox"
+                className={css`
+                  cursor: pointer;
+                  user-select: none;
+                  padding: 0.5rem;
+                  margin: -0.5rem;
+
+                  ${mediaQueries.medium`
+                display: none;
+              `};
+                `}
+              >
+                {/* eslint-enable jsx-a11y/label-has-for */}
+                <FaChevronUp />
+                <FaChevronDown />
+              </label>
+
+              <nav
+                className={css`
+                  @media (max-width: 768px) {
+                    width: 100%; /* Wrap component onto a new line */
+                  }
+                `}
+              >
+                <ul
+                  className={css`
+                    display: flex;
+                    list-style-type: none;
+                    padding: 0;
+                    margin: 0;
+
+                    ${mediaQueries.medium`
                   & > li {
                     margin-left: 3rem;
                   }
                 `};
-              `}
-            >
-              {[['/#videos', 'Videók'], ['/#gallery', 'Galéria'], ['/#sponsors', 'Támogatók']].map(([to, name]) => (
-                <li key={to}>
-                  <Link to={to} onClick={() => this.setState({ isMenuOpen: false })}>
-                    {name}
-                  </Link>
-                </li>
-                ))}
-            </ul>
-          </nav>
-        </Container>
+                  `}
+                >
+                  {[
+                    ['/#videos', 'Videók'],
+                    ['/#gallery', 'Galéria'],
+                    ['/#sponsors', 'Támogatók'],
+                  ].map(([to, name]) => (
+                    <li key={to}>
+                      <Link to={to} onClick={() => this.setState({ isMenuOpen: false })}>
+                        {name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </Container>
+          </div>
+        </Headroom>
       </header>
     );
   }
