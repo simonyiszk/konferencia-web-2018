d5a488a27535d0302cf528ac69d67613945cebd4
Kristóf Poduszló <kripod@protonmail.com>
2018-01-30 20:16:27 +0100

    Crop album thumbnails on demand with GraphQL

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -178,7 +178,7 @@ export const query = graphql`
             source
             thumbnail {
               childImageSharp {
-                sizes(maxWidth: 688) {
+                sizes(maxWidth: 690, maxHeight: 460, cropFocus: EAST) {
                   ...GatsbyImageSharpSizes
                 }
               }
