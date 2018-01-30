8b409c61dac7a0444b599cafc0513084c0b0bfa4
Kristóf Poduszló <kripod@protonmail.com>
2018-01-30 22:48:30 +0100

    Improve layout of the 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { css } from 'react-emotion';
+import Helmet from 'react-helmet';
+import Container from '../components/Container';
 
 const NotFoundPage = () => (
-  <div>
+  <Container
+    className={css`
+      h1 {
+        text-align: left;
+      }
+    `}
+  >
+    <Helmet title="Hiba" />
+
     <h1>Az oldal nem található</h1>
     <p>A kért oldal jelenleg nem elérhető vagy nem létezik.</p>
-  </div>
+  </Container>
 );
 
 export default NotFoundPage;
