32d3658b1a597f2718a7a757a200a394ff9ca247
Kristóf Poduszló <kripod@protonmail.com>
2018-02-18 18:43:57 +0100

    Improve layout of the 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import Container from '../components/Container';
+import PageContentContainer from '../components/PageContentContainer';
 
 const NotFoundPage = () => (
-  <Container>
+  <PageContentContainer>
     <Helmet title="Hiba" />
 
     <h1>Az oldal nem található</h1>
-    <p>A kért oldal jelenleg nem elérhető vagy nem létezik.</p>
-  </Container>
+    <p className="text-center">
+      A kért oldal jelenleg nem elérhető vagy nem létezik.
+    </p>
+  </PageContentContainer>
 );
 
 export default NotFoundPage;
