70ab80a9d8194e192cfb3df396867d66ad973867
Kristóf Poduszló <kripod@protonmail.com>
2018-02-25 01:21:12 +0100

    Add support for navbar space allocation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,30 +4,34 @@ import Container from './Container';
 import styles from './Navbar.module.scss';
 
 const Navbar = ({
-  brand, children, className, ...props
+  brand, children, allocateSpace, className, ...props
 }) => (
-  <div className={`${styles.root} ${className}`} {...props}>
-    <Container className={styles.mainContainer}>
-      <div className={styles.brandContainer}>{brand()}</div>
+  <div style={allocateSpace && { marginBottom: '4rem' }}>
+    <div className={`${styles.root} ${className}`} {...props}>
+      <Container className={styles.mainContainer}>
+        <div className={styles.brandContainer}>{brand()}</div>
 
-      <nav>
-        <ul className={styles.navItemsContainer}>
-          {React.Children.map(children, navLink => (
-            <li className={styles.navItem}>{navLink}</li>
-          ))}
-        </ul>
-      </nav>
-    </Container>
+        <nav>
+          <ul className={styles.navItemsContainer}>
+            {React.Children.map(children, navLink => (
+              <li className={styles.navItem}>{navLink}</li>
+            ))}
+          </ul>
+        </nav>
+      </Container>
+    </div>
   </div>
 );
 
 Navbar.propTypes = {
   brand: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
+  allocateSpace: PropTypes.bool,
   className: PropTypes.string,
 };
 
 Navbar.defaultProps = {
+  allocateSpace: true,
   className: '',
 };
 
