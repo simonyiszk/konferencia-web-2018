27234def1e1baf3348b342047bcbbdd93f434c2e
Kristóf Poduszló <kripod@protonmail.com>
2018-04-04 12:42:56 +0200

    Make presentation abstracts collapsible

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,45 +1,85 @@
 import Img from 'gatsby-image';
 import PropTypes from 'prop-types';
 import React from 'react';
+import FaChevronDown from 'react-icons/lib/fa/chevron-down';
+import FaChevronUp from 'react-icons/lib/fa/chevron-up';
 import styles from './Presentation.module.scss';
 
-const Presentation = ({
-  title,
-  presenterName,
-  presenterRole,
-  presenterImage,
-  time,
-  location,
-  abstract,
-  className,
-  ...props
-}) => (
-  <article className={`${styles.root} ${className}`} {...props}>
-    <div className={styles.presenterImageContainer}>
-      <Img resolutions={presenterImage} className={styles.presenterImage} />
-    </div>
+export default class Presentation extends React.Component {
+  constructor() {
+    super();
 
-    <div className={styles.timeAndLocationContainer}>
-      <h2 className={styles.time}>{time}</h2>
-      <p className={styles.location}>{location}</p>
-    </div>
+    this.state = {
+      isAbstractCollapsed: true,
+    };
 
-    <div>
-      <h2 className={styles.title}>{title}</h2>
-      {presenterName != null && (
-        <p className={styles.presenterInfo}>
-          {presenterName} – {presenterRole}
-        </p>
-      )}
+    this.handleAbstractTogglerClick = this.handleAbstractTogglerClick.bind(this);
+  }
 
-      <p
-        className={styles.abstract}
-        /* eslint-disable-next-line react/no-danger */
-        dangerouslySetInnerHTML={{ __html: abstract }}
-      />
-    </div>
-  </article>
-);
+  handleAbstractTogglerClick() {
+    this.setState(prevState => ({
+      isAbstractCollapsed: !prevState.isAbstractCollapsed,
+    }));
+  }
+
+  render() {
+    const {
+      title,
+      presenterName,
+      presenterRole,
+      presenterImage,
+      time,
+      location,
+      abstract,
+      className,
+      ...props
+    } = this.props;
+    const { isAbstractCollapsed } = this.state;
+
+    return (
+      <button
+        type="button"
+        aria-expanded="false"
+        aria-label="Absztrakt mutatása/elrejtése"
+        className={`${styles.root} ${className}`}
+        onClick={this.handleAbstractTogglerClick}
+        {...props}
+      >
+        <article className={styles.article}>
+          <div className={styles.presenterImageContainer}>
+            <Img
+              resolutions={presenterImage}
+              className={styles.presenterImage}
+            />
+          </div>
+
+          <div className={styles.timeAndLocationContainer}>
+            <h2 className={styles.time}>{time}</h2>
+            <p className={styles.location}>{location}</p>
+          </div>
+
+          <div className={styles.presentationDetailsContainer}>
+            <h2 className={styles.title}>{title}</h2>
+            {presenterName != null && (
+              <p className={styles.presenterInfo}>
+                {presenterName} – {presenterRole}
+              </p>
+            )}
+
+            <p
+              /* eslint-disable-next-line react/no-danger */
+              dangerouslySetInnerHTML={{ __html: abstract }}
+              className={styles.abstract}
+              style={isAbstractCollapsed ? { display: 'none' } : {}}
+            />
+          </div>
+
+          <h2>{isAbstractCollapsed ? <FaChevronDown /> : <FaChevronUp />}</h2>
+        </article>
+      </button>
+    );
+  }
+}
 
 Presentation.propTypes = {
   title: PropTypes.string.isRequired,
@@ -57,5 +97,3 @@ Presentation.defaultProps = {
   presenterRole: null,
   className: '',
 };
-
-export default Presentation;
