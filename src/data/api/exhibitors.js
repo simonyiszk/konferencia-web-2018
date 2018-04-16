046ecbef5efa786a4b68dc53a564eb372e820cad
Kristóf Poduszló <kripod@protonmail.com>
2018-04-16 13:44:30 +0200

    Render exhibitor data statically instead of client-side fetching

---
+++ src/data/api/exhibitors.js
@@ -0,0 +1,68 @@
+export default [
+  {
+    id: 1,
+    name: 'Vodafone',
+    description:
+      'Gyere, látogass el a standunkhoz, ahol megtudhatod, hogy hogyan használjuk a drónokat a munkánk megkönnyítéséhez. Ráadásul magát a drónt is megnézheted! Érdeklődj a cégről, az aktuális nyitott pozíciókról és töltsd ki a kvízünket, hogy megtudhasd, milyen digitális pozíciók illenének hozzád.',
+  },
+  {
+    id: 2,
+    name: 'HA5KFU',
+    description:
+      'A HA5KFU a Simonyi Károly Szakkollégium rádióamatőr köre. Körünk a hagyományos rádióamatőr tevékenység mellett foglalkozik hardveres illetve szoftverrádiós fejlesztésekkel. A standon az érdeklődök a rádióamatőrök tevékenysége mellett a kör szoftverrádiós projektjeivel ismerkedhetnek meg.',
+  },
+  {
+    id: 3,
+    name: 'NI',
+    description:
+      'Gyere és éld át a tudományt az NI standjánál! Termékeinkből készített demonk segítségével megismerkedhetsz cégünk munkájával és kollégáinktól tájékozódhatsz mindennapi életünkről! Játssz velünk a LabVIEW-ban írt játékainkkal és kamatoztasd az egyetemi órákon megszerzett tudásod!',
+  },
+  {
+    id: 4,
+    name: 'Kir-Dev',
+    description:
+      'Webfejlesztés, mobilfeljesztés. Ez a két szó az, ami legjobban jellemzi a Kir-Dev munkásságát. Ha érdekelnek ezeknek a területeknek a legfrissebb fejleményei, akkor náluk érdemes tájékozódnod. Legyen szó Node.js, IonicFramework vagy éppen RubyOnRails rejtelmeiről, náluk megtalálhatod a szükséges információkat.',
+  },
+  {
+    id: 5,
+    name: 'OTP',
+    description:
+      'Az OTP Bank standjánál a látogatók tájékoztatást kaphatnak számos munkakörről, gyakornoki programról. Természetesen a szórakozás sem maradhat el! A játékos kedvűek egy virtuális ügyességi játékban csillogtathatják meg tudásukat. Sok szeretettel várunk Téged is!',
+  },
+  {
+    id: 6,
+    name: 'SEM',
+    description:
+      'Standunknál kipróbálhatod saját készítésű áramköreinket, illetve megtekinthetsz néhányat a sikeres projektjeink közül. Érdekel, hogy miként lehet Etherneten monitorozni egy mosógép működését? Kíváncsi vagy mit lehet elérni, ha az egyetem mellett ön szorgalomból is foglalkozol szakmával? Téged is várunk a standunknál!',
+  },
+  {
+    id: 7,
+    name: 'Bosch',
+    description:
+      'Érdekel a mobilitás jövője? A mesterséges intelligencia vagy az önvezető autók? Az E-moblitás? Beszélgess a Bosch mérnökeivel, fedezd fel a legújabb innovációk fejlesztési titkait, tudd meg hogyan válik egy ötletből valóra, hogyan jut el a fejlesztéstől a gyártásig egy-egy termék.\n<br>Találd meg a Hozzád leginkább passzoló pozíciót, a legfrissebb ajánlatokkal várnak a cég HR-es kollégái.',
+  },
+  {
+    id: 8,
+    name: 'schdesign',
+    description:
+      'Csapatunk nagy hangsúlyt fektet az esztétikus, de egyben könnyen használható felhasználói felületek kialakítására, sokatmondó arculatok létrehozására, művészi grafikák megtervezésére. Projektjeink során az előbb felsorolt témakörökön belül törekszünk arra, hogy mindig valami maradandót alkossunk!',
+  },
+  {
+    id: 9,
+    name: 'Lego',
+    description:
+      'Standunknál az érdeklődők betekintést nyerhetnek a robotika és minden ahhoz kapcsolódó terület részleteibe. Projektjeink mind szoftveres, mind hardveres részét hallgatók tervezik és kivitelezik, így nem csak innovatív megoldásokat, de gyakran határterületi érdekességeket is tudunk mutatni. Témáink közé tartozik a mesterséges intelligencia, gépi látás, űrtechnológia és sok más.',
+  },
+  {
+    id: 10,
+    name: 'Nokia',
+    description:
+      'Látogass el a Nokia Bell Labs standjához és ismerd meg, milyenek lesznek a jövő telekommunikációs hálózatai és tudd meg, mi is az a Future X! Érdekes demó is vár rád, amely bemutatja, hogy egy okostelefon és gépi tanulás segítségével hogyan észlelhetőek a felhasználó által átélt közlekedési események és hogyan lehet ezt a telekommunikációs hálózat adataival kombinálva „okossá” tenni a közlekedést.',
+  },
+  {
+    id: 11,
+    name: 'Accenture',
+    description:
+      'Kíváncsi vagy rá, hogy gyakorlatban, hogyan alkalmazzuk a Digital Thread-et, és milyen nálunk a Smart Factory? A standunkon betekintést nyerhetsz a virtuális és a kiterjesztett valóságba, valamint testközelből figyelheted meg az ipari automatizálást egy complex gyártósori modellen keresztül. A HTC Vive eszközünkön pedig szabadon kipróbálhatod egy gumigyártó üzem gyártási folyamatát és egy specifikus szervizszituációt (a gumifelület lebontását végző célszerszám biztonságos cseréjének folyamatát a robotizált gyártókörnyezetben).',
+  },
+];
